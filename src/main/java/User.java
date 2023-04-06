2ecc8920361a7e817a904403da6bcaaae3d52c1a
linakat23 <lina.katkute1@gmail.com>
2023-04-06 10:15:46 +0300

    Test draft

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import java.util.List;
 
 public class User {
@@ -19,20 +18,43 @@ public class User {
     }
 
     public static boolean registerUser(User user) {
-    driver.findElement(By.xpath("//*[@id=\"form\"]/fieldset/table/tbody/tr[10]/td/p/a")).click();
-    driver.findElement(By.id("UserName")).sendKeys(user.username);
-    driver.findElement(By.id("Email")).sendKeys(user.email);
-    driver.findElement(By.id("Password")).sendKeys(user.password);
-    driver.findElement(By.id("Password2")).sendKeys(user.password2);
-    driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[11]/td[2]/input")).click();
-    return true;
+        driver.findElement(By.xpath("//*[@id=\"form\"]/fieldset/table/tbody/tr[10]/td/p/a")).click();
+        driver.findElement(By.id("UserName")).sendKeys(user.username);
+        driver.findElement(By.id("Email")).sendKeys(user.email);
+        driver.findElement(By.id("Password")).sendKeys(user.password);
+        driver.findElement(By.id("Password2")).sendKeys(user.password2);
+        driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[11]/td[2]/input")).click();
+        return checkRegistrationStatus();
     }
 
-//    public static boolean checkRegistrationFromStatus() {
-//        boolean output = true;
-//
-//        if (driver.findElements(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[1]/td[2]/span")).isEmpty()) {
-//        }
-//        return true;
-//    }
+    public static boolean checkRegistrationStatus() {
+        boolean output = true;
+
+        List<WebElement> usernameEmptyError = driver.findElements(By.xpath("/html/body/div[1]/form/fieldset/table/tbody/tr[1]/td[2]/span"));
+        List<WebElement> usernameExistsError = driver.findElements(By.xpath("/html/body/div[1]/form/fieldset/table/tbody/tr[1]/td[2]/span"));
+        List<WebElement> invalidEmailError = driver.findElements(By.xpath("/html/body/div[1]/form/fieldset/table/tbody/tr[4]/td[2]/span"));
+        List<WebElement> passwordTooShortError = driver.findElements(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[7]/td[2]/span"));
+
+        if (usernameEmptyError.size() > 0) {
+            System.out.println(usernameEmptyError.get(0).getText());
+            return false;
+        }
+
+        if (usernameExistsError.size() > 0) {
+            System.out.println(usernameExistsError.get(0).getText());
+            return false;
+        }
+
+        if (invalidEmailError.size() > 0) {
+            System.out.println(invalidEmailError.get(0).getText());
+            return false;
+        }
+
+        if (passwordTooShortError.size() > 0) {
+            System.out.println(passwordTooShortError.get(0).getText());
+            return false;
+        }
+
+        return output;
+    }
 }
--- src/test/java/LentaRegisterTest.java
+++ src/test/java/LentaRegisterTest.java
@@ -6,25 +6,25 @@ import static org.testng.Assert.*;
 import java.time.Duration;
 
 public class LentaRegisterTest {
-    public WebDriver driver;
 
-    @Test
-    public void registerUserTest(){
+    @Test(priority = 1)
+    public void registerUserTest() {
         assertTrue(User.registerUser(new User("Jonas", "jonasjonaitis11@yahoo.com", "testavimas", "testavimas")));
     }
-//    @Test
-//    public void registerUserUsernameTakenTest(){
-//        assertTrue(User.registerUser(new User("Jonas", "jonasjonaitis11@yahoo.com", "testavimas", "testavimas")));
-//    }
+
+    @Test
+    public void multipleErrorsTest() {
+        assertTrue(User.registerUser(new User("", "jonasjonaitis11", "aaaa", "aaaa")));
+    }
 
     @BeforeClass
     public void beforeClass() {
-        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver112.exe");
+       // System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver112.exe");
         User.driver = new ChromeDriver();
         User.driver.manage().window().maximize();
         User.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        driver.get("https://elenta.lt/prisijungti?returnurl=https%3A%2F%2Felenta.lt%2F");
-        driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[1]/div[2]/div[2]/button[1]/p")).click();
+        User.driver.get("https://elenta.lt/prisijungti?returnurl=https%3A%2F%2Felenta.lt%2F");
+        User.driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[1]/div[2]/div[2]/button[1]/p")).click();
 
     }
 
